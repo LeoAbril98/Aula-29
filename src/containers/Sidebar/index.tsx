bc9ef4e578d72d1dff1fa5d4b8c915958cc8ad0a
Leonardo Abril <leoabril98@gmail.com>
2024-03-01 06:34:16 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import { Descricao, BotaoTema, SideBarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/leoabril98.png" />
-    <Titulo fontSize={20}>Leonardo Abril</Titulo>
+    <SideBarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Leonardo Abril</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        leoabril98
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro Front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SideBarContainer>
   </aside>
 )
 
